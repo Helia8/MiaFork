77db66a91c0656c8f278c0cf204e10bffb21f382
Helia <soulhood2.0@gmail.com>
2025-10-26 01:33:08 +0200

    feat: boilerplate quest stuff

---
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/quests/QuestFeature.kt
@@ -0,0 +1,17 @@
+package com.mineinabyss.features.quests
+
+import com.mineinabyss.features.abyss
+import com.mineinabyss.idofront.features.Configurable
+import com.mineinabyss.idofront.features.FeatureWithContext
+import com.mineinabyss.idofront.config.config
+import com.mineinabyss.idofront.features.FeatureDSL
+
+class QuestFeature : FeatureWithContext<QuestFeature.Context>(::Context) {
+    class Context: Configurable<QuestConfig> {
+        override val configManager = config("quests", abyss.dataPath, QuestConfig())
+    }
+
+    override fun FeatureDSL.enable() {
+       //
+    }
+}
---
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/quests/QuestListener.kt
@@ -0,0 +1,14 @@
+package com.mineinabyss.features.quests
+
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerMoveEvent
+
+class QuestListener: Listener {
+
+    @EventHandler
+    fun PlayerMoveEvent.onLocationEnter() {
+        //
+    }
+}
