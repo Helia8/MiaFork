58ea660392ecb031296ac930ba5ecc382fe996df
user <soulhood2.0@gmail.com>
2025-10-27 13:31:22 +0100

    refactor: unused import

--- mineinabyss-features/src/main/kotlin/com/mineinabyss/features/quests/LocationsConfig.kt
+++ mineinabyss-features/src/main/kotlin/com/mineinabyss/features/quests/LocationsConfig.kt
@@ -3,7 +3,6 @@ package com.mineinabyss.features.quests
 import com.mineinabyss.idofront.serialization.LocationSerializer
 import kotlinx.serialization.Serializable
 import org.bukkit.Location
-import org.bukkit.WorldBorder
 
 // List of all the locations that are to be detected by quests
 
@@ -14,7 +13,7 @@ class LocationData(
     val locationCenter: @Serializable(with = LocationSerializer::class) Location,
     val radius: Int,
 ) {
-//    var visited : Boolean = false
+    //    var visited : Boolean = false
     val minX = locationCenter.blockX - radius
     val maxX = locationCenter.blockX + radius
     val minY = locationCenter.blockY - radius
@@ -23,7 +22,7 @@ class LocationData(
     val maxZ = locationCenter.blockZ + radius
 
     fun isInside(location: Location, flat: Boolean = false): Boolean {
-        return  location.blockX in minX..maxX  && location.blockZ in minZ..maxZ && (flat || location.blockY in minY..maxY)
+        return location.blockX in minX..maxX && location.blockZ in minZ..maxZ && (flat || location.blockY in minY..maxY)
     }
 
 }
@@ -32,6 +31,6 @@ class LocationData(
 class LocationsConfig(
     val locations: Map<String, LocationData> = emptyMap(),
 
-) {
+    ) {
 }
 
